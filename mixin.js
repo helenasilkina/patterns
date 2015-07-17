2bf5d97a4d252b6df506edf19790467b0e6aed72
Helena Silkina <helenasilkina@gmail.com>
2015-07-17 13:30:16 +0300

    Create mixin.js

---
+++ mixin.js
@@ -0,0 +1,25 @@
+/*
+** Copy any objects, mixing their properties in new object 
+*
+* Tests
+* var cake = mix{
+*   {eggs: 2, large: true},
+*   {butter: 1, salted: true},
+*   {flour: "3 cups"},
+*   {sugar: "sure!"}
+* }
+*
+* mixin(cake);
+*/
+
+function mixin() {
+  var child = {};
+  for (var i = 0; arg < arguments.length; i++) {
+    for ( var property in arguments[i]) {
+      if (arguments[i].hasOwnproperty(property)) {
+        child[property] = arguments[i][property];
+      }
+    }
+  }
+  return child;
+}

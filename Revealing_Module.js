5cfac1c741dae6818b969f6b1a0b50cc3ea49c6a
Helena Silkina <helenasilkina@gmail.com>
2015-07-19 13:58:20 +0300

    Update Revealing_Module.js

--- Revealing_Module.js
+++ Revealing_Module.js
@@ -1,5 +1,26 @@
 /*
-* The idea here is that you have private methods which you want to expose as public methods 
+* The idea here is that you have private methods which you want to expose as public methods.
+* What are doing below is effectively difining a self-executing function and immediately returning.
 *
+* Tests with
+* myRevealingModule.get();* 
 * @author: Addy Osmani
 */
+
+var myRevealingModule = (function() {
+  var name = "Alex Good";
+  var age = 25;
+  function updatePerson() {
+    name = "Alex Good update"
+  }
+  function setPerson() {
+    name = "Alex Better";
+  }
+  function getPerson() {
+    return name;
+  }
+  return {
+    get: getPerson,
+    set: setPerson
+  }
+})();

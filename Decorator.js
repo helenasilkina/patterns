450edddb77220e564457de2805868c73cd2a69c3
Helena Silkina <helenasilkina@gmail.com>
2015-07-21 22:27:37 +0300

    Update Decorator.js

--- Decorator.js
+++ Decorator.js
@@ -1,7 +1,32 @@
 /*
 * Tests
-* var myMacbook = new Instance(new BlurayDrive(new Memory(new Mackbook())));
+* var myMacbook = new Insurance(new BlurayDrive(new Memory(new Mackbook())));
 * console.log(myMackbook.cost());
 */
 
 "use strict";
+function Macbook() {
+  this.cost = function() {
+    return 1000;
+  }
+}
+
+function Memory(macbook) {
+  this.cost = function() {
+    return macbook.cost() + 75;
+  }
+}
+
+function BlurayDrive(macbook) {
+  this.cost = function() {
+    return macbook.cost() + 300;
+  }
+}
+
+function Insurance(macbook) {
+  this.cost = function() {
+    return macbook.cost() + 100;
+  }
+}
+
+

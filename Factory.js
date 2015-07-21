e5d0de1daeb765668425492b6ed1b1a25e2958ca
Helena Silkina <helenasilkina@gmail.com>
2015-07-21 22:23:27 +0300

    Update Factory.js

--- Factory.js
+++ Factory.js
@@ -3,3 +3,15 @@
 * var civic = new Car("Honda Civic", 2009, 20000);
 * var civic = new Car("Ford Mondeo", 2010, 5000);
 */
+
+"use strict";
+var Car = (function(){
+  var Car= function(model, year, miles) {
+    this.model = model;
+    this.year = year;
+    this.miles = miles;
+  };
+  return function(model, year, miles) {
+    return new Car(model, year, miles); 
+  };
+})();

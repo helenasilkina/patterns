ead901218868d71565300b13406301ea2352890d
Helena Silkina <helenasilkina@gmail.com>
2015-07-21 22:19:32 +0300

    Update Facade.js

--- Facade.js
+++ Facade.js
@@ -1,1 +1,11 @@
 "use strict";
+var addMyEvent = function (el, ev, fn) {
+  if (el.addEventListener {
+    el.addEventListener(ev, fn, false);
+  } else if (el.attachEvent) {
+    el.attachEvent('on'+ev, fn);
+  } else {
+    el['on'+ev] = fn;
+  }
+};
+
